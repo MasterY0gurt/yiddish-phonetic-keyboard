5a22c99f12a50e7f4c91d93241833939b984005d
MasterY0gurt <94236819+MasterY0gurt@users.noreply.github.com>
2021-11-21 12:29:16 +0100

    Update update.sh

--- Linux/update.sh
+++ Linux/update.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
-# based on the installer by https://github.com/nevillepark/yiddish-keys-linux/blob/master/install.sh
-
-##########################################################################
-
-# Update the layout file in the layout directory
-sudo cp yi /usr/share/X11/xkb/symbols/yi &&
-
-# Say nice things
-echo 'Update succesfull!'
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+while true; do
+    read -p "Do you wish to update the yiddish keyboard layout? (y/n) " yn
+    case $yn in
+        [Yy]* )
+                # Update the layout
+                sudo cp yi /usr/share/X11/xkb/symbols/yi;
+                break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+echo "Update successful!"
